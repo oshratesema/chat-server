ee83cd9cb8fd0ed644f1be68acaeb302b89a7590
Oshrat Tesema <100847911+oshratesema@users.noreply.github.com>
2022-11-24 20:07:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const socket = require('socket.io')
+
+const userRouters = require('./routers/usersRouters')
+const msgRouters = require('./routers/messagesRoute')
+
+const app = express()
+require('dotenv').config();
+
+app.use(cors())
+app.use(express.json());
+
+app.use('/api/auth', userRouters)
+app.use("/api/messages", msgRouters);
+
+mongoose.connect(process.env.MONGO, {
+    useNewUrlParser : true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('DB Connection Successfully');
+}).catch((err) => {
+    console.log(err.message);
+})
+
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    console.log('sendMsg', {data});
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.message);
+    }
+  });
+});
